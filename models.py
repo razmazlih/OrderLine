1c94ec69629c1ff678c89e6e44738f6abb081b38
Raz <retraz123@gmail.com>
2024-12-06 15:20:37 +0200

    update delete default total_paice=0.0

--- models.py
+++ models.py
@@ -9,5 +9,5 @@ class Order(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, nullable=False, index=True)
     restaurant_id = Column(Integer, nullable=False, index=True)
-    total_price = Column(Float, nullable=False, default=0.0)
+    total_price = Column(Float, nullable=False)
     ordered_at = Column(DateTime, default=None)
