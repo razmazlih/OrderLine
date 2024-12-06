a77e79937b00a4f14b7ada62a3fcc276527d8a32
Raz <retraz123@gmail.com>
2024-12-06 17:06:42 +0200

    add schemas to OrderItemModel

---
+++ schemas/order_item_schemas.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, field_validator
+from typing import Optional, Required
+
+
+class CreateOrderItemSchema(BaseModel):
+    order_id: int
+    menu_item_id: int
+    quantity: int
+    price: float
+
+    @field_validator("quantity")
+    def validate_quantity(cls, value):
+        if value < 0:
+            raise ValueError("Quantity must be greater than or equal to 0")
+        return value
+
+    @field_validator("price")
+    def validate_price(cls, value):
+        if value < 0:
+            raise ValueError("Price must be greater than or equal to 0")
+        return value
+
+
+class UpdateOrderItemSchema(BaseModel):
+    quantity: Optional[int]
+    price: Optional[float]
+
+
+class OrderItemResponseSchema(BaseModel):
+    id: Optional[int] = None
+    order_id: int
+    menu_item_id: int
+    quantity: int
+    price: float
