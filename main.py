fb1bb54aff918efbbb465fbe4537fc8a5b81f16e
Raz <retraz123@gmail.com>
2024-12-10 19:50:55 +0200

    update the routes in app

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from init_db import init_db
 from models import Base
 from routes.order_routes import order_router
 from routes.order_item_routes import order_item_router
+from routes.order_status_websocket import order_status_ws_router
+from routes.order_status_routes import order_status_router
 
-# יצירת בסיס הנתונים
 init_db()
 
 app = FastAPI()
 
-# רישום הראוטר
 app.include_router(order_router)
 app.include_router(order_item_router)
-
+app.include_router(order_status_ws_router)
+app.include_router(order_status_router)
