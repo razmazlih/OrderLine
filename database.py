4025bf88dd8fb603aeef806a234bbaf3f513243b
Raz <retraz123@gmail.com>
2024-12-13 22:08:08 +0200

    change db to postgressql

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from os import getenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./my_data.db"
+
+DATABASE_USERNAME= getenv("DATABASE_USERNAME", "database-name")
+DATABASE_PASSWORD=getenv("DATABASE_PASSWORD", "password")
+DATABASE_NAME=getenv("DATABASE_NAME", "db-name")
+DATABASE_HOST=getenv("DATABASE_HOST", "localhost")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
